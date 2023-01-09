c1ef7956e756dd6494b287f8b6b77a7f4048ebac
Tony <bowen.anth@gmail.com>
2023-01-09 10:43:39 -0700

    changed imagelinkform.js button type to submit

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -12,6 +12,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
           <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
           <button
             className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+            type='submit'
             onClick={onButtonSubmit}
           >Detect</button>
         </div>
